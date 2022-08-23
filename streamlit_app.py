0367201ae9a9c6ea3d57cc8e6824cbd23fedbf3e
Prosenjit Sarkar <111803972+prosenjitaccount@users.noreply.github.com>
2022-08-23 11:00:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
-streamlit.title('My first py code deployed at Streamlit')
+streamlit.title('My Mom''s Dinner')
+                
+streamlit.header('Healthy Breakfast Menu')   
+streamlit.text(' Egg - Scrambbled or boiled') 
+streamlit.text(' Toast - Slice with butter or jelly') 
+streamlit.text(' Milk or Cofee or Tea - with sugar 1 or 2') 
+streamlit.header('** End **') 
