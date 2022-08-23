f57b18d287506d384e47b413a6917298c0f602af
Prosenjit Sarkar <111803972+prosenjitaccount@users.noreply.github.com>
2022-08-23 11:03:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,5 @@
 import streamlit
+import pandas
 
-streamlit.title('My Mom''s Dinner')
-                
-streamlit.header('Healthy Breakfast Menu')   
-streamlit.text(' Egg - Scrambbled or boiled') 
-streamlit.text(' Toast - Slice with butter or jelly') 
-streamlit.text(' Milk or Cofee or Tea - with sugar 1 or 2') 
-streamlit.header('** End **') 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
