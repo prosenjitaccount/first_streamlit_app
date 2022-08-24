95e8fb14836954a62d80b9da2061859672fe7c87
Prosenjit Sarkar <111803972+prosenjitaccount@users.noreply.github.com>
2022-08-24 16:51:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response.json())
 
  
+# write your own comment -what does the next line do? - Formatting the data frame
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do? - Showing formatted the data frame
+streamlit.dataframe(fruityvice_normalized)
